6503d25eb6040270c2ab27b97c92b5427e644bad
diana1014 <65572068+diana1014@users.noreply.github.com>
2020-09-26 00:06:47 -0500

    Update code_review1.py

--- code_review1.py
+++ code_review1.py
@@ -21,40 +21,43 @@ def dices () :
 #Main::::::::::::::::::::::::::::::::::
 
 suma_t=[]
-cont_pares= 0
-cont_inpares= 0
+contador_pares= 0
+contador_impares= 0
 
 os.system("cls")
-numero=int(input("::: Introducir el numero de veces a lanzar: "))
+numero=int(input("***Ingresar el numero de veces a lanzar: "))
 if (numero>=1):
-    i = 1
-    while i <= numero :
-        print("Tiro Nro: ", i)
+    D = 1
+    while D <= numero :
+        print("Tiro Numero: ", D)
         
         dd = dices()
         suma_t.append(dd[2])
-        print("d1: ", dd[0])        
-        print("d2: ", dd[1])
+        print("Dado1: ", dd[0])        
+        print("Dado2: ", dd[1])
         print("Total: ",dd[2])
-        i = i + 1
+        D = D + 1
         if dd[2]%2==0:
-            cont_pares+=1
+            contador_pares+=1
         else:
-            cont_inpares+=1
+            contador_impares+=1
         if dd[0] &dd[1]==6:
             break
             
-        key = input("==Lanzar nuevamente ==:::")
+        key = input("==Lanzar nuevamente ==")
         if(dd[0]==6 and dd[1]==6):
            print("Dados iguales")
            break
         
 
     print("*********************************************")
-    print(":: Valor total de lanzamientos: ",sum(suma_t))
-    print(":: Total de pares: ",cont_pares)
-    print(":: Total de impares: ", cont_inpares)
+    print("*** Valor total de lanzamientos: ",sum(suma_t))
+    print("*** Total de pares: ",contador_pares)
+    print("*** Total de impares: ", contador_impares)
+    print("*********************************************")
 else:
     print("El numero ingresado debe de ser mayor o igual a 1")
+
+
 
 
